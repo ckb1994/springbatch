0720ebbcdaaf913186a6f364e19c51f4949697bd
chandan kumar <kumarc313@gmail.com>
2020-01-21 23:22:55 +0530

    First Commit done

---
+++ src/test/java/com/robin/springbatcheample1/SpringBatchEample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robin.springbatcheample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchEample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
